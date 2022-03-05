f6f47c4127096cf3fa38d0c1cfd581e19e55936f
CarlosAS10 <carlosas08082003@gmail.com>
2022-03-05 13:15:19 +0100

    Fix #1

--- Tarea 4.2 EDD/src/calculadora/Main.java
+++ Tarea 4.2 EDD/src/calculadora/Main.java
@@ -33,18 +33,6 @@ public class Main {
 		System.err.print("\n\nEl programa ha terminado.");
 	}
 	
-	public static void suma() {
-		
-		Scanner sc = new Scanner(System.in);
-				
-		System.out.print("\nPrimer número: ");
-			int a = sc.nextInt();
-		System.out.print("Segundo número: ");
-			int b = sc.nextInt();
-	
-		System.out.printf("\n%d + %d = %d", a, b, (a + b));
-	}
-	
 	public static void resta() {
 		
 		Scanner sc = new Scanner(System.in);
@@ -55,6 +43,18 @@ public class Main {
 			int b = sc.nextInt();
 	
 		System.out.printf("\n%d - %d = %d", a, b, (a - b));
+	}
+	
+	public static void suma() {
+		
+		Scanner sc = new Scanner(System.in);
+				
+		System.out.print("\nPrimer número: ");
+			int a = sc.nextInt();
+		System.out.print("Segundo número: ");
+			int b = sc.nextInt();
+	
+		System.out.printf("\n%d + %d = %d", a, b, (a + b));
 	}
 	
 	public static void multi() {
