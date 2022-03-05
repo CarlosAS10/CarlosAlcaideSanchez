b6c6011e2da6e4ba9af34fcffb2a2e3d5059e430
CarlosAS10 <carlosas08082003@gmail.com>
2022-03-05 12:21:45 +0100

    0.1

---
+++ README.md
@@ -0,0 +1,8 @@
+# README
+
+Proyecto: Calculadora
+Autor: Carlos Alcaide Sánchez
+Versión: 0.1
+
+Descripción: 
+El proyecto está creado en una sola clase y formado por métodos. El programa hace de calculadora. Se selecciona una opción y se introducen 2 números, el programa dará un resultado dependiendo de la opción que se haya seleccionado.
---
+++ Tarea 4.2 EDD/src/calculadora/Main.java
@@ -0,0 +1,53 @@
+package calculadora;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int option;
+		do {
+			System.out.print("\n\n0.- Salir \n1.- Suma \n2.- Resta");
+			System.out.print("\n\nOpción: ");
+				option = sc.nextInt();
+				
+			switch(option) {
+				case 1:
+					suma();
+					break;
+				case 2:
+					resta();
+					break;
+			}
+		} while(option != 0);
+		
+		System.err.print("\n\nEl programa ha terminado.");
+	}
+	
+	public static void suma() {
+		
+		Scanner sc = new Scanner(System.in);
+				
+		System.out.print("\nPrimer número: ");
+			int a = sc.nextInt();
+		System.out.print("Segundo número: ");
+			int b = sc.nextInt();
+	
+		System.out.printf("\n%d + %d = %d", a, b, (a + b));
+	}
+	
+	public static void resta() {
+		
+		Scanner sc = new Scanner(System.in);
+				
+		System.out.print("\nPrimer número: ");
+			int a = sc.nextInt();
+		System.out.print("Segundo número: ");
+			int b = sc.nextInt();
+	
+		System.out.printf("\n%d - %d = %d", a, b, (a - b));
+	}
+}
